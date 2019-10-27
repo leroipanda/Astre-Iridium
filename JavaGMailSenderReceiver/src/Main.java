1130cbe928656738e87025bc5e3962eb6b82e1e9
Louis <louis.blenner@gmail.com>
2019-10-27 22:32:19 +0100

    Changefile

---
+++ JavaGMailSenderReceiver/src/Main.java
@@ -0,0 +1,18 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //final String username = "louis.blenner";
+        //final String password = ""; //Mon mdp d'application Pas touche
+
+        MainProcess go = new MainProcess();
+        go.routine();
+
+    }
+
+}
---
+++ JavaGMailSenderReceiver/src/MainProcess.java
@@ -0,0 +1,50 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class MainProcess {
+
+    private String username;
+    private String mdp;
+    private String dest;
+    Scanner scan = new Scanner(System.in);
+
+    public MainProcess(){
+
+    }
+
+    public void routine() {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        while(true) {
+            System.out.print("Entrez votre adresse gmail: ");
+            username = scan.next();
+            System.out.print("Entrez votre mot de passe application : ");
+            mdp = scan.next();
+
+            ReadEmail reader = new ReadEmail(username);
+
+            reader.read(username,mdp);
+
+            System.out.print("Adresse destination : ");
+            dest = scan.next();
+            System.out.print("Sujet : ");
+            String suj = scan.next();
+            System.out.print("Message : ");
+            String msg = scan.next();
+
+            SendMail sender = new SendMail();
+            sender.send(username,mdp,dest,suj,msg);
+
+            System.out.println("Succés");
+
+        }
+    }
+}
